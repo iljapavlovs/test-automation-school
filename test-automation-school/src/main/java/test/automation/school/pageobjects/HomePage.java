2585feb2ef9a11f175734cca4b426757bbd46aee
ilja <ilja.pavlovs>
2017-05-17 10:44:25 +0300

    initial commit

---
+++ test-automation-school/src/main/java/test/automation/school/pageobjects/HomePage.java
@@ -0,0 +1,50 @@
+package test.automation.school.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(xpath = "//*[@href='/ru/']")
+    public WebElement switchLangToRuLink;
+    @FindBy(xpath = "//*[@href='/lv/']")
+    public WebElement switchLangToLvLink;
+    @FindBy(xpath = "//*[@class='a1'][@href='/ru/electronics/']")
+    public WebElement goToElectronicsLink;
+
+    @FindBy(xpath = "//img[@class='page_header_logo']")
+    public WebElement pageHeaderLogo;
+
+    @FindBy(xpath = "//*[@class='page_header_head']/h1")
+    public WebElement pageHeaderHead;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.elementToBeClickable(pageHeaderLogo));
+    }
+
+    public HomePage switchLangTo(String language) {
+        switch (language) {
+            case "LV":
+                if (switchLangToLvLink.isDisplayed()) {
+                    switchLangToLvLink.click();
+                    break;
+                }
+            case "RU":
+                if (switchLangToRuLink.isDisplayed()) {
+                    switchLangToRuLink.click();
+                }
+        }
+        return this;
+    }
+}
+
+
---
+++ test-automation-school/src/test/java/test/automation/school/BaseTest.java
@@ -0,0 +1,61 @@
+package test.automation.school;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import test.automation.school.utils.ConfigReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class BaseTest {
+
+    private static final String APPLICATION_URL = ConfigReader.getConfigValue("application.url");
+
+    WebDriver driver;
+
+    @BeforeSuite(alwaysRun = true)
+    public void beforeSuite() {
+//        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+
+        File file = new File("C:\\tools\\Firefox\\", "firefox.exe");
+        if (file.exists()) {
+            System.setProperty("webdriver.firefox.bin", "C:\\tools\\Firefox\\firefox.exe");
+        }
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup(){
+//        driver = new ChromeDriver();
+
+        FirefoxOptions options = new FirefoxOptions().setLegacy(true);
+        driver = new FirefoxDriver(options);
+
+        driver.get(APPLICATION_URL);
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void takeScreenShotOnFailure(ITestResult testResult) throws IOException {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("target" + File.separator
+                    + "errorScreenshots" + File.separator
+                    + testResult.getName() + "-"
+                    + new SimpleDateFormat("yyyy-MM-dd_HHmm").format(new Date()) + ".png"));
+        }
+
+        driver.quit();
+    }
+}
