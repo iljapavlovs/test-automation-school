132d14c2b5982afd1ab034c8946b6b1c25bb2ce9
S1EAMK <4Sisma123sr>
2017-10-17 17:18:45 +0300

    update

--- module1/src/test/java/test/automation/school/classexamples/NavigateToUrl.java
+++ module1/src/test/java/test/automation/school/classexamples/NavigateToUrl.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NavigateToUrl {
     public static void main(String[] args) {
+        // If Firefox is installed other than in 'Program Files' folder, then Selenium needs to know where to find the Firefox binary file (firefox.exe)
+        System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+        // Selenium needs to know wehere to find Browser`s Driver. Sets by 'webdriver.<driver name>.driver' env variable
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-v2.30-win32/chromedriver.exe");
 
         // when we create new webdriver instance then a new browser is opened depending on which driver was used (e.g. in this case Chrome)
