ad91612527c6a404ba1a35784ada3f6b0928aa27
S1EAMK <4Sisma123sr>
2017-10-18 17:22:28 +0300

    added some hints

--- module6/src/test/java/atbildes/LocatorsTest.java
+++ module6/src/test/java/atbildes/LocatorsTest.java
@@ -14,11 +14,28 @@ import java.util.List;
 
 
 public class LocatorsTest {
+    /*
+     * add assert for all tests - check for visibility
+     * in order to run individual test, you need to hover over the required test and right click ->Run
+     * check that variables named correctly
+     * import correct List
+     * use findElements for retrieving multiple elements, not findElement
+     * css selector ->
+        * .class .class - 'space' means nesting (one element is a children or grandchildren of another)
+        * .class.class - when written in conjunction then it means that this element has multiple classes and
+            * in HTML it is viewed via space 'class class'
+     * 2nd task - search for all elements, not possible to search for visible
+     * 6th and 7th - search only for visible elements
+     * linkText() is case sensitive and should be selected as it is rendered on the screen, not in HTML
+     * dont be afraid to check the stacktrace, it contains useful info - e.g. the line of code where it has failed
+     * Use debug to see in which state AUT is at the moment of failure
+     * consider how browser is initialized - in this example it is initialized once and killed once, so there is only one browser session for all tests
+     */
     private WebDriver driver;
 
     @BeforeClass
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-v2.30-win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "../drivers/chromedriver-v2.30-win32/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
     }
@@ -40,7 +57,7 @@ public class LocatorsTest {
 
     @Test
     public void locateElementByClassNameTest() throws Exception {
-        //TODO 2 - locate all products by class name and put them into ArrayList Collection
+        //TODO 2 - locate *all* products by class name and put them into ArrayList Collection
         List<WebElement> elementByClassNameList = driver.findElements(By.className("ajax_block_product"));
 
         Assert.assertTrue(elementByClassNameList.get(0).isDisplayed());
