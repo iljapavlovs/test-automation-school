161c98fafab475bbf853d3da17e5747e839a7769
vt1987 <valentina.tonko@ctco.lv>
2018-05-31 13:32:56 +0300

    -- New test

---
+++ module1/src/test/java/test/automation/school/New.java
@@ -0,0 +1,24 @@
+package test.automation.school;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class New {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\repo\\vt selenium\\test-automation-school-master\\drivers\\chromedriver-v2.39-win32\\chromedriver.exe");
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("http://www.google.com");
+//        String baseUrl = "https://www.google.com";chromeDriver.get(baseUrl);
+//        chromeDriver.findElement(By.name("q"));
+        WebElement searchField = chromeDriver.findElement(By.cssSelector("[name='q']"));
+//        chromeDriver.findElement(By.xpath("//*[@[name='q']]"));
+
+        searchField.sendKeys("Hello");
+        searchField.sendKeys(Keys.ENTER);
+
+    }
+}
