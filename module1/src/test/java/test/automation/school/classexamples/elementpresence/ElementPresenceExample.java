9d056613658d604820a9eeac12fad53d2093536c
vt1987 <valentina.tonko@ctco.lv>
2018-06-04 23:02:32 +0300

    -- Locator test HW

--- module1/src/test/java/test/automation/school/classexamples/elementpresence/ElementPresenceExample.java
+++ module1/src/test/java/test/automation/school/classexamples/elementpresence/ElementPresenceExample.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ElementPresenceExample {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-v2.30-win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-v2.39-win32/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/dynamic_loading/1");
 
